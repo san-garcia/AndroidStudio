3baf88030f623d9b5f0c09cbe668595c0b7fffdf
saangar <sandragarciafabra@gmail.com>
2025-02-06 13:50:52 +0100

    Proyecto final foto de perfil

---
+++ ProyectoFinal/app/src/main/java/com/example/proyectofinal/Inicio.kt
@@ -0,0 +1,14 @@
+package com.example.proyectofinal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class Inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inicio)
+
+    }
+}
